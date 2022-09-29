588070c6a22a181406bcd0216d0f1fd190aa43a7
uddinmayraj <mayrajuddin03@gmail.com>
2022-09-29 15:31:09 +0600

    responsive done

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -18,46 +18,48 @@ const Layout = () => {
 
     }
     return (
-        <div className='row'>
-            <div className='activity col-9 '>
-                <div className='top-header'>
-                    <h1> UtRA-Active-club</h1>
+        <div className='d-flex'>
+            <div className='col-9'>
+                <div className="activity">
+                    <div className='top-header'>
+                        <h1> UtRA-Active-club</h1>
+                    </div>
+                    <h3>Select today's exercise</h3>
+                    <Workouts addTime={addTime}></Workouts>
                 </div>
-                <h3>Select today's exercise</h3>
-                <Workouts addTime={addTime}></Workouts>
             </div>
             <div className='details col-3 '>
-                <div className='px-2 px-md-3 details-child'>
-                    <div className='py-3 d-md-flex align-items-center'>
+                <div className='details-child mx-1 mx-md-3'>
+                    <div className='py-3 text-center d-md-flex align-items-center'>
                         <span className='me-3 fs-1'><FontAwesomeIcon icon={faUserTie}></FontAwesomeIcon></span>
                         <div className=''>
-                            <h5>Mayraj Uddin</h5>
+                            <h5 className='fs-t'>Mayraj Uddin</h5>
                             <FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon>
                             <span className='ms-1'> Mirsarai, Bangladesh</span>
                         </div>
                     </div>
-                    <div className='mb-3 cc pd rounded parson-detail d-md-flex justify-content-between'>
-                        <span>58 <small>kg</small></span>
-                        <span >5.3 <small>height</small></span>
-                        <span>22 <small>yrs age</small></span>
+                    <div className='mb-3 cc pd rounded parson-detail text-center d-md-flex justify-content-between'>
+                        <span className='fs d-block mb-2'>58 <small>kg</small></span>
+                        <span className='fs d-block mb-2' >5.3 <small>height</small></span>
+                        <span className='fs d-block'>22 <small>yrs age</small></span>
                     </div>
-                    <p className=' fs-md-5 text-capitalize fw-bold'> Add a break</p>
-                    <div className='mb-4 cc rounded break pd d-md-flex justify-content-between'>
-                        <button className='mb-2'>5<small>min</small></button>
-                        <button className='mb-2'>10<small>min</small></button>
-                        <button className='mb-2'>15<small>min</small></button>
-                        <button className='mb-2'>30<small>min</small></button>
+                    <p className=' fs-md-5 text-capitalize fw-bold fs-t'> Add a break</p>
+                    <div className='mb-4 cc rounded break pd text-center d-md-flex justify-content-between'>
+                        <button className='mb-2 px-3 px-md-2'>5<small>min</small></button>
+                        <button className='mb-2 px-3 px-md-2'>10<small>min</small></button>
+                        <button className='mb-2 px-3 px-md-2'>15<small>min</small></button>
+                        <button className='mb-2 px-3 px-md-2'>30<small>min</small></button>
                     </div>
-                    <h5>Exercise Details</h5>
-                    <div className='mb-4 cc rounded pd d-md-flex justify-content-between'>
-                        <span>Exercise time</span>
+                    <h5 className='fs-t text-center'>Exercise Details</h5>
+                    <div className='mb-4 cc rounded pd text-center d-md-flex justify-content-between'>
+                        <span className='d-block d-md-inline fs fs-md-5'>Exercise time</span>
                         <span className='px-4'> {totalTime}s</span>
                     </div>
-                    <div className='mb-4 cc rounded pd d-md-flex justify-content-between'>
-                        <span>Break time</span>
+                    <div className='mb-4 cc rounded pd text-center d-md-flex justify-content-between'>
+                        <span className='d-block d-md-inline fs'>Break time</span>
                         <span className='px-4'><Utilities time={3}></Utilities></span>
                     </div>
-                    <button className='w-100 py-3 btn  btn-primary rounded'>Activity Completed</button>
+                    <button className='w-100 py-3 btn  btn-primary rounded fs-t'>Activity Completed</button>
                 </div>
             </div>
         </div>
